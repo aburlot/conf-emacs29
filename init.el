757f09acba8c9a283f0e81618ea95c43580c29f3
Alan BURLOT <alan.burlot@proton.me>
2024-05-28 07:19:49 +0200

    ajout moody

--- init.el
+++ init.el
@@ -14,8 +14,12 @@
  '(custom-safe-themes
    '("6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(package-selected-packages
-   '(auctex gptel yasnippet-snippets yasnippet eldoc-box mastodon all-the-icons-ibuffer flymake-yaml yaml-mode affe org-caldav markdown-preview-mode ellama dired consult-dir markdown-mode all-the-icons all-the-icons-completion all-the-icons-dired marginalia corfu embark-consult embark orderless vertico ein which-key csv highlight-indent-guides dired-x wgrep doom-modeline rainbow-delimiters pyvenv color-theme-sanityinc-tomorrow magit))
- '(safe-local-variable-values '((after-save-hook . org-babel-tangle)))
+   '(moody cc-isearch-menu flymake-yamllint casual casual-dired highlight-symbol async org-ref auto-package-update auctex gptel yasnippet-snippets yasnippet eldoc-box mastodon all-the-icons-ibuffer flymake-yaml yaml-mode affe org-caldav markdown-preview-mode ellama dired consult-dir markdown-mode all-the-icons all-the-icons-completion all-the-icons-dired marginalia corfu embark-consult embark orderless vertico ein which-key csv highlight-indent-guides dired-x wgrep doom-modeline rainbow-delimiters pyvenv color-theme-sanityinc-tomorrow magit))
+ '(safe-local-variable-values
+   '((org-export-in-background . t)
+     (org-latex-default-packages-alist "")
+     (reftex-default-bibliography "~/PHILOSOPHIE/NOTES/BiblioPhilo.bib")
+     (after-save-hook . org-babel-tangle)))
  '(use-package-always-defer t)
  '(use-package-always-ensure t))
 (custom-set-faces
