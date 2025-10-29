078b8537a5f89f9eb2074b61b3fff933d3c7a405
Alan BURLOT <alan.burlot@proton.me>
2025-10-29 22:14:09 +0100

    add early-init.el

---
+++ early-init.el
@@ -0,0 +1,5 @@
+;; Remove earlier the graphical stuff
+(message "enters early init")
+(menu-bar-mode -1) ;; Cache la barre de menu
+(scroll-bar-mode -1) ;; Supprimer les scroll bars
+(tool-bar-mode -1) ;; Cache la barre d'outils
